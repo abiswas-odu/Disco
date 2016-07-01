0d86e8675db38795d39a157ace0e76c80eddec30
Abhishek Biswas <biswasa@ornl.gov>
2016-07-01 16:24:59 -0700

    added smaller test case

--- test/runEcoli.sh
+++ test/runEcoli.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-../runOmega3.sh -d . -inP ./Ecoli_250_500.fna -n 4 -m 5 -o ecoli -p ../omega3.cfg
+../runOmega3.sh -d . -inP ./Ecoli_250_500_test.fna -n 4 -m 5 -o ecoli -p ../omega3.cfg
 
