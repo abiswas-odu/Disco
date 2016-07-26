a335e10a95254b4bc969ca571a3711df0a471f17
Abhishek Biswas <biswasa@ornl.gov>
2016-07-26 09:49:09 -0400

    Changed initial simplification for debugging...

--- src/SimplifyGraph/src/OverlapGraph.cpp
+++ src/SimplifyGraph/src/OverlapGraph.cpp
@@ -1102,10 +1102,10 @@ void OverlapGraph::graphPathFindInitial(vector<string> containedReadsFileName)
 		<< " and clip branches with very short overlap length.\n";
 	// Composite edge contraction with remove dead end nodes
 	m_dataset->storeContainedReadInformation(containedReadsFileName);
-	calculateMeanAndSdOfInnerDistance();
+	//calculateMeanAndSdOfInnerDistance();
 	UINT64 counter(0);
 	do {
-		findSupportByMatepairsAndMerge();
+		//findSupportByMatepairsAndMerge();
 		removeDeadEndNodes();
 		counter = contractCompositeEdgesPar();
 	} while (counter > 0);
