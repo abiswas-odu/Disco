f52972b2889b1519f481dcbbcbf2b45bb5ed379e
Abhishek Biswas <biswasa@ornl.gov>
2016-08-08 16:52:58 -0400

    fixed buildG MPI

--- src/BuildGraphMPI/Release/sources.mk
+++ src/BuildGraphMPI/Release/sources.mk
@@ -23,6 +23,6 @@ C_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-MetaGenomics \
-MetaGenomics/Debug \
+BuildGraphMPI \
+BuildGraphMPI/src \
 
--- src/BuildGraphMPI/Release/src/subdir.mk
+++ src/BuildGraphMPI/Release/src/subdir.mk
@@ -1,7 +1,7 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
 ################################################################################
-
+CC := mpic++
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS_BUILDG_MPI += \
 ../../src/Dataset.cpp \
