2e12fe02e04b977aa08b800c22e3ecc28e7c4c10
Abhishek Biswas <biswasa@ornl.gov>
2017-01-06 15:17:51 -0500

    Fixed MPI scripts

--- runDisco-MPI-SLURM.sh
+++ runDisco-MPI-SLURM.sh
@@ -163,10 +163,10 @@ outSimplifyPrefix="${dataOutPath}/assembly/${outPrefix}"
 
 logFile="${dataOutPath}/${outPrefix}.log"
 #Create edge file list
-i=0
 j=0
 while [ $j -lt $numProcs ]
 do
+   i=1
    while [ $i -lt $numThreads ]
    do
       edgeFiles="${edgeFiles}${dataOutPath}/graph/${outPrefix}_${j}_${i}_parGraph.txt,"
@@ -177,10 +177,10 @@ done
 edgeFiles=${edgeFiles%?}
 
 #Create contained read file list
-i=0
 j=0
 while [ $j -lt $numProcs ]
 do
+   i=1
    while [ $i -lt $numThreads ]
    do
       containedReads="${containedReads}${dataOutPath}/graph/${outPrefix}_${j}_${i}_containedReads.txt,"
