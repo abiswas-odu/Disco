cf0baa75fc5d3fc6dfc520f917cccf558d4a2225
Abhishek Biswas <biswasa@ornl.gov>
2017-05-24 17:15:22 -0400

    change config

--- src/SimplifyGraph/src/main.cpp
+++ src/SimplifyGraph/src/main.cpp
@@ -137,17 +137,11 @@ bool SimplifyGraph(const vector<std::string> &read_SingleFiles,const vector<std:
 		overlapGraph = new OverlapGraph(parGlobalGraph, simplifyPartialPath, dataSet,
 						minOvl, threadPoolSize);
 	}
-
-	UINT64 uniCount=0;
+	UINT64 uniCount=1;
 	string untig_file = outputFilenamePrefix+"_unitigs_"+SSTR(interationCount)+".fasta";
-	//string untig_aln1_file = outputFilenamePrefix+"_unitigAln1.sam";
 	overlapGraph->streamUnitigs(read_SingleFiles,read_PairFiles, read_PairInterFiles,untig_file, "unitig",uniCount);
-	//overlapGraph->printAllEdges(outputFilenamePrefix+"_CurrGraph_.txt");
-	//string runBlasr="blasr -noRefineAlign -advanceHalf -noSplitSubreads -minMatch 10 -sdpTupleSize 7 " \
-			"-minPctIdentity 70 -bestn 10 -sam -clipping soft -nproc 16 -header ";
-	//runBlasr += untig_file + " pbio-523.5374.fasta -out " + untig_aln1_file;
-    //system(runBlasr.c_str());
-    //MYEXIT("Unitig printed and stopped.");
+	overlapGraph->printAllEdges(outputFilenamePrefix+"_CurrGraph_.txt");
+	//MYEXIT("Unitig printed and stopped.");
 	//Initial Simplification
 	if(checkPointParams[interationCount-1][1]==0)		//Check if initial simplification complete
 	{
